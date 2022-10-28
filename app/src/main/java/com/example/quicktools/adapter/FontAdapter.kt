d99c77327440cd64c7a43c01f57753bf5a1ff905
priyesh bhuva0 <bhuvapriyesh9@gmail.com>
2022-10-28 15:18:07 +0530

    done

--- app/src/main/java/com/example/quicktools/adapter/FontAdapter.kt
+++ app/src/main/java/com/example/quicktools/adapter/FontAdapter.kt
@@ -1,23 +1,68 @@
 package com.example.quicktools.adapter
 
-import android.view.View
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.content.Context.CLIPBOARD_SERVICE
+import android.content.Intent
+import android.content.SharedPreferences
+import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.AdapterView.OnItemClickListener
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.example.quicktools.databinding.FontLayoutBinding
+import com.example.quicktools.model.Font
 
-class FontAdapter() : RecyclerView.Adapter<FontAdapter.FontHolder>() {
-    class FontHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
+class FontAdapter(
+    private val context: Context,
+    private val dataset: ArrayList<Font>,
+    onItemClickListener: OnItemClickListener
+) : RecyclerView.Adapter<FontAdapter.FontHolder>() {
+    var preferences: SharedPreferences? = null
+
+    class FontHolder(itemView: FontLayoutBinding) : RecyclerView.ViewHolder(itemView.root) {
+        val binding = itemView
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FontHolder {
-        TODO("Not yet implemented")
+        val binding = FontLayoutBinding.inflate(LayoutInflater.from(context), parent, false)
+        return FontHolder(binding)
     }
 
     override fun getItemCount(): Int {
-       return  0 
+        return dataset.size
     }
 
     override fun onBindViewHolder(holder: FontHolder, position: Int) {
-        TODO("Not yet implemented")
+
+        holder.binding.tvStlishText.text = dataset[holder.adapterPosition].fontText
+
+        holder.binding.layoutInsta.setOnClickListener {
+            val whatsappIntent = Intent(Intent.ACTION_SEND)
+            whatsappIntent.type = "text/plain"
+            whatsappIntent.setPackage("com.whatsapp")
+            whatsappIntent.putExtra(Intent.EXTRA_TEXT, holder.binding.tvStlishText.text)
+            context.startActivity(whatsappIntent)
+        }
+
+        holder.binding.ivShare.setOnClickListener {
+            val intent2 = Intent()
+            intent2.action = Intent.ACTION_SEND
+            intent2.type = "text/plain"
+            intent2.putExtra(Intent.EXTRA_TEXT, holder.binding.tvStlishText.text)
+            context.startActivity(Intent.createChooser(intent2, "Share via"))
+        }
+
+        holder.binding.ivCopy.setOnClickListener {
+            (holder.itemView.context.getSystemService(CLIPBOARD_SERVICE) as ClipboardManager).setPrimaryClip(
+                ClipData.newPlainText(
+                    "stylish text", holder.binding.tvStlishText.text
+                )
+            )
+            Toast.makeText(holder.itemView.context, "Text Copied", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/quicktools/model/Font.kt
@@ -0,0 +1,5 @@
+package com.example.quicktools.model
+
+ class Font{
+    lateinit var fontText : String
+}

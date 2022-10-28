e7b7453ea0278488a6e4282f656e5dcb50a39a07
priyesh bhuva0 <bhuvapriyesh9@gmail.com>
2022-10-28 12:53:27 +0530

    Initial commit

---
+++ app/src/main/java/com/example/quicktools/adapter/FontAdapter.kt
@@ -0,0 +1,23 @@
+package com.example.quicktools.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class FontAdapter() : RecyclerView.Adapter<FontAdapter.FontHolder>() {
+    class FontHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FontHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+       return  0 
+    }
+
+    override fun onBindViewHolder(holder: FontHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
